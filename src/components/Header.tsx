91409b7ffae1570dacbd26468af238ad5ec443ac
kauan777 <kauan7622@gmail.com>
2022-07-29 13:47:43 -0300

    adcionando estilizações

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,40 @@
 export function Header() {
   return (
     <header className='
-     w-full text-gray-50 font-extralight px-16 py-8 text-center
+     w-full text-gray-50 font-light px-16 py-8 text-center
      flex justify-between'
      >
         <h4 className='py-2 px-4 rounded-full'>Apple</h4>
         <ul className='flex gap-2 items-center'>
-          <li className='cursor-pointer py-2 px-4 rounded-full'>Home</li>
-          <li className='cursor-pointer py-2 px-4 rounded-full'>About</li>
-          <li className='cursor-pointer py-2 px-4 rounded-full'>Services</li>
-          <li className='cursor-pointer py-2 px-4 rounded-full'>Sign up</li>
+          <li className='
+            cursor-pointer
+            py-2 px-4 rounded-full 
+            hover:bg-slate-50
+            transition-all
+            hover:text-gray-500
+            '>Home</li>
+                      <li className='
+            cursor-pointer
+            py-2 px-4 rounded-full 
+            hover:bg-slate-50
+            transition-all
+            hover:text-gray-500
+            '>Products</li>
+                      <li className='
+            cursor-pointer
+            py-2 px-4 rounded-full 
+            hover:bg-slate-50
+            transition-all
+            hover:text-gray-500
+            '>Contact</li>
+          <li className='
+                    cursor-pointer 
+                    border border-solid 
+                    transition-all
+                  border-white py-2 px-4 rounded-full
+                    hover:bg-gray-50
+                    hover:text-gray-500
+                  '>Sign in</li>
         </ul>
     </header>
   )
